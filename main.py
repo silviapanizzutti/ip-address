0d683b5c2244d86fd7e58085208bea4b003c7bcf
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-11-30 10:10:30 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from ip_address import get_location
-
-ip_address = '153.138.24.18'
-
-country, city = get_location(ip_address)
-
-print("IP address {} is from {}, {} ".format(ip_address, city,  country))
