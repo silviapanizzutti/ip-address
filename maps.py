737718936a947f5ca8934c916beba31bc1502fad
jacopobre <74046050+jacopobre@users.noreply.github.com>
2020-12-05 18:27:13 +0100

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,11 +1,17 @@
-
+import plotly.graph_objects as go
 import plotly.express as px
+import pandas as pd # hover_name="City", hover_data=["State", "Population"]
+def show_map(data):
+    fig = px.scatter_mapbox(pd.DataFrame(data=data), lat="lat", lon="lon",
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=1200)
+    fig = go.Figure(data=go.Scattergeo(
+        lat = data["lat"],
+        lon = data["lon"],
+        mode = 'lines',
+        line = dict(width = 2, color = 'blue'),
+    ))
 
-data = {'lat':[45.5, 54.4],
-        'lon':[12.3, 17.9]}
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-
-fig = px.scatter_mapbox(data, lat="lat", lon="lon", color_discrete_sequence=["blue"], zoom=3, height=800)
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+    fig.show()
