53806347dd6c68d84de62e725cd52a26374b15db
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-07 10:01:15 +0100

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,10 +1,5 @@
-
-
 from geopy import distance
 
-def getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2): 
-    return distance.distance((lat1,lon1),(lat2,lon2)).km
 
-
-
-
+def getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2):
+    return distance.distance((lat1, lon1), (lat2, lon2)).km
