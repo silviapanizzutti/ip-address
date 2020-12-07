cd49482345f3a033d90a2fb9edb4e50ccaf2181a
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-07 10:10:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from ip_address import get_location
-from distance import getDistanceFromLatLonInKm
+from distance import getDistanceFromLatLonInKm as getdistlat
 import argparse
 import sys
+
+
 def getOptions(args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
@@ -9,16 +11,21 @@ def getOptions(args=sys.argv[1:]):
     parser.add_argument("-ip", "--ip_address", help="Put the ip address")
     args = parser.parse_args()
     return args
-args=getOptions()
-if  args.ip_address is None:
-    ip_address=input("give me an ip address ")
+args = getOptions()
+if args.ip_address is None:
+    ip_address = input("give me an ip address ")
 else:
     ip_address = args.ip_address
 try:
-    lat1,lng1,lat2,lng2,country, city = get_location(ip_address)
+    lat1, lng1, lat2, lng2, country, city = get_location(ip_address)
 except:
     if args.verbose:
         print("ip address is not valid")
     quit()
-print("IP address {} is from {}, {} {:10.3f}Km".format(ip_address, city,  country,getDistanceFromLatLonInKm(lat1,lng1,lat2,lng2)))
-
+print("IP address {} is from {}, {} {:10.3f}Km".format(ip_address,
+                                                       city,
+                                                       country,
+                                                       getdistlat(lat1,
+                                                                  lng1,
+                                                                  lat2,
+                                                                  lng2)))
