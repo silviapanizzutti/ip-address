15c974bbebb207be0aea96de7ef44d4a5d7516c3
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-07 12:05:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from ip_address import get_location
-from distance import getDistanceFromLatLonInKm
+from distance import getDistanceFromLatLonInKm as getdistlat
 import argparse
 from maps import show_map
 import sys
+
 
 def getOptions(args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
@@ -11,24 +12,28 @@ def getOptions(args=sys.argv[1:]):
     parser.add_argument("-ip", "--ip_address", help="insert the ip address")
     args = parser.parse_args()
     return args
-args=getOptions()
-    
-    
-if  args.ip_address is None:
-    ip_address=input("insert an ip address ")
+args = getOptions()
+
+
+if args.ip_address is None:
+    ip_address = input("insert an ip address ")
 else:
     ip_address = args.ip_address
 try:
-    lat1,lng1,lat2,lng2,country, city = get_location(ip_address)
+    lat1, lng1, lat2, lng2, country, city = get_location(ip_address)
 except:
     if args.verbose:
         print("ip address is not valid")
     quit()
 try:
-    show_map({"lat":[lat1,lat2],"lon":[lng1,lng2]})
+    show_map({"lat": [lat1, lat2], "lon": [lng1, lng2]})
 except:
     import traceback
     traceback.print_exc()
-print("IP address {} is from {}, {} {:10.3f}Km".format(ip_address, city,  country,getDistanceFromLatLonInKm(lat1,lng1,lat2,lng2)))
-
-
+print("IP address {} is from {}, {} {:10.3f}Km".format(ip_address,
+                                                       city,
+                                                       country,
+                                                       getdistlat(lat1,
+                                                                  lng1,
+                                                                  lat2,
+                                                                  lng2)))
