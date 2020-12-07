d5b51c046b6f7f0f6fc4a890f5a96e2868df1849
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-07 10:29:14 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 import sqlite3
 import hashlib
-import secrets 
+import secrets
 import argparse
 conn = None
 cursor = None
+
 
 def open_and_create():
     global conn
@@ -13,45 +14,36 @@ def open_and_create():
     try:
         cursor.execute("SELECT * FROM user")
     except sqlite3.OperationalError:
-        
+
         cursor.execute('''CREATE TABLE user
                      (username TEXT, password TEXT, salt TEXT,
                       PRIMARY KEY (username))''')
 
+
 def save_new_username_correct(username, password):
     global conn
     global cursor
-
-    
-    salt = secrets.token_hex(16) 
-    digest = hashlib.sha256(password.encode('utf-8') + salt.encode("utf-8") ).hexdigest()
-
-    
+    salt = secrets.token_hex(16)
+    digest = hashlib.sha256(password.encode('utf-8') + salt.encode("utf-8")).hexdigest()
     cursor.execute("INSERT OR REPLACE INTO user VALUES (?,?,?)",
-                   (username, digest,salt))
+                   (username, digest, salt))
     conn.commit()
 
 
 def check_for_username_correct(username, password):
     global conn
     global cursor
-    rows = cursor.execute("SELECT salt FROM user WHERE username=?",[username])
-    
+    rows = cursor.execute("SELECT salt FROM user WHERE username=?", [username])
     salt = rows.fetchall()
-    if len(salt) == 0 :
+    if len(salt) == 0:
         return -1
-    
     digest = hashlib.sha256(password.encode('utf-8') + salt[0][0].encode("utf-8")).hexdigest()
-
-    
     rows = cursor.execute("SELECT * FROM user WHERE username=? and password=?",
                           (username, digest))
     conn.commit()
     results = rows.fetchall()
-    
     if results:
         print("User is present, password is valid")
         return 1
     else:
         return -1
-
