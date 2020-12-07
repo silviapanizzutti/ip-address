cfe87564f1d58487b9cb6e892d6a3b051c80a386
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-07 10:03:07 +0100

    Update ip_address.py

--- ip_address.py
+++ ip_address.py
@@ -2,22 +2,23 @@ import requests
 import json
 
 
-IP_URL ='http://ip-api.com/json/{}'
+IP_URL = 'http://ip-api.com/json/{}'
+
 
 def get_location(ip_address):
-    re=requests.get("http://ip-api.com/json/")
-    loc=re.json()
+    re = requests.get("http://ip-api.com/json/")
+    loc = re.json()
     URL = IP_URL.format(ip_address)
     r = requests.get(URL)
     info = json.loads(r.text)
     try:
         location = info['country']
         city = info['city']
-        lat1=info["lat"]
-        lng1=info["lon"]
-        lat2=loc["lat"]
-        lng2=loc["lon"]
+        lat1 = info["lat"]
+        lng1 = info["lon"]
+        lat2 = loc["lat"]
+        lng2 = loc["lon"]
     except KeyError:
         raise KeyError
 
-    return lat1,lng1,lat2,lng2,location, city
+    return lat1, lng1, lat2, lng2, location, city
