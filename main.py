1323792d251ed5d338539dfdded24539229ea785
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-07 12:12:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,50 +5,59 @@ import argparse
 from getpass import getpass
 import sys
 
+
 def getOptions(args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                         action="store_true")
     parser.add_argument("-ip", "--ip_address", help="insert the ip address")
-    parser.add_argument('-u', "--username", help="insert a usernamename (requires -p)",
+    parser.add_argument('-u',
+                        "--username",
+                        help="insert a usernamename (requires -p)",
                         required=False)
-    parser.add_argument('-p',"--password", help="the username's password",
+    parser.add_argument('-p', "--password", help="the username's password",
                         required=False)
     args = parser.parse_args()
     return args
-args=getOptions()
+args = getOptions()
 
 open_and_create()
-#save_new_username_correct("admin","admin")
+# save_new_username_correct("admin","admin")
 if args.username is None:
-    user=input("insert username ")
+    user = input("insert username ")
     passwd = getpass()
-    chec= check_for_username_correct(user,passwd)
+    chec = check_for_username_correct(user, passwd)
     if chec == -1:
         print("Password not valid")
         while chec != 1:
-            user=input("insert a username ")
-            passwd=input ("insert a password ")
-            chec= check_for_username_correct(user,passwd)
+            user = input("insert a username ")
+            passwd = input("insert a password ")
+            chec = check_for_username_correct(user, passwd)
 else:
     if args.password is None:
         print("insert the password")
         quit(0)
-    user=args.username
-    passwd=args.password
-    chec= check_for_username_correct(user,passwd)
+    user = args.username
+    passwd = args.password
+    chec = check_for_username_correct(user, passwd)
     if chec == -1:
         quit(1)
-if  args.ip_address is None:
-    ip_address=input("insert an ip address ")
+if args.ip_address is None:
+    ip_address = input("insert an ip address ")
 else:
     ip_address = args.ip_address
 try:
-    lat1,lng1,lat2,lng2,country, city = get_location(ip_address)
+    lat1, lng1, lat2, lng2, country, city = get_location(ip_address)
 except:
     if args.verbose:
         print("ip address is not valid")
     quit()
 
 
-print("IP address {} is from {}, {} {:10.3f}Km".format(ip_address, city,  country,getDistanceFromLatLonInKm(lat1,lng1,lat2,lng2)))
+print("IP address {} is from {}, {} {:10.3f}Km".format(ip_address,
+                                                       city,
+                                                       country,
+                                                       getdistlat(lat1,
+                                                                  lng1,
+                                                                  lat2,
+                                                                  lng2)))
