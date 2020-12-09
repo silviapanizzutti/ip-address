d2c79c5e522eb4d6e118d51a6ede0b8e50a5f958
silviapanizzutti <74045150+silviapanizzutti@users.noreply.github.com>
2020-12-09 17:13:24 +0100

    Update main.py
    I added the possibility for the user to choose between sign in and sign up, so that if it is the first time he can create a username and password while if it is not he can get in with his credentials

--- main.py
+++ main.py
@@ -18,31 +18,43 @@ def getOptions(args=sys.argv[1:]):
                         required=False)
     parser.add_argument('-p', "--password", help="the username's password",
                         required=False)
+    parser.add_argument('-s',"--signup", help="are you a new user?select 2, otherwise press 1",
+                        required=False)
     args = parser.parse_args()
     return args
 args = getOptions()
 
-open_and_create()
-# save_new_username_correct("admin","admin")
-if args.username is None:
-    user = input("insert username ")
-    passwd = getpass()
-    chec = check_for_username_correct(user, passwd)
-    if chec == -1:
-        print("Password not valid")
-        while chec != 1:
-            user = input("insert a username ")
-            passwd = input("insert a password ")
+if args.signup is None:
+    signup=input("press 1 if you want to sign in and 2 if you want to sign up ")
+    if signup == "2":
+        open_and_create()
+        user=input("insert a new username ")
+        passwd=input("insert a new password ")
+        save_new_username_correct(user,passwd)
+    elif  signup == "1":
+        if args.username is None:
+            user = input("insert username ")
+            passwd = getpass()
+            open_and_create()
             chec = check_for_username_correct(user, passwd)
-else:
-    if args.password is None:
-        print("insert the password")
-        quit(0)
-    user = args.username
-    passwd = args.password
-    chec = check_for_username_correct(user, passwd)
-    if chec == -1:
-        quit(1)
+            if chec == -1:
+                print("Password not valid")
+                while chec != 1:
+                    user = input("insert a username ")
+                    passwd = input("insert a password ")
+                    chec = check_for_username_correct(user, passwd)
+        else:
+            if args.password is None:
+                print("insert the password")
+                quit(0)
+                user = args.username
+                passwd = args.password
+                chec = check_for_username_correct(user, passwd)
+            if chec == -1:
+                quit(1)
+    else:
+        print("you just put a wrong input ")
+        quit()
 if args.ip_address is None:
     ip_address = input("insert an ip address ")
 else:
