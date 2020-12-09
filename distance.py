46f7db1b252c3e74c6ded844012895c963d624e1
silviapanizzutti <74045150+silviapanizzutti@users.noreply.github.com>
2020-12-09 18:01:11 +0100

    Update distance.py

--- distance.py
+++ distance.py
@@ -2,4 +2,4 @@ from geopy import distance
 
 
 def getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2):
-    return distance.distance((lat1, lon1), (lat2, lon2)).km
+    return distance.distance((lat1, lon1), (lat2, lon2)).km  # this function measures the difference in km from one ip to the other using their coordinates
