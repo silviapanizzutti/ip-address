f6465f0e9a5999edf94bd1e355054df4d68bc397
valeriapiccoli <74241699+valeriapiccoli@users.noreply.github.com>
2020-12-11 23:24:00 +0100

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,20 +1,23 @@
 import unittest
-from ip_address  import get_location
+from ip_address import get_location
+
 
 class TestIPAddress(unittest.TestCase):
-# IPTest: valid inputs
+    # IPTest: valid inputs
     def test_correct_values(self):
         # you should select some valid inputs, for which the output is known
-        *other, location,city = get_location("153.138.24.18")
-        self.assertEqual(location,"Japan")
-# invalid inputs
+        *other, location, city = get_location("153.138.24.18")
+        self.assertEqual(location, "Japan")
+
+    # invalid inputs
     def test_wrong_values(self):
         # you should input wrong data
-        self.assertRaises(KeyError,lambda:get_location("20") )
-# corner case: empty string
+        self.assertRaises(KeyError, lambda: get_location("20"))
+
+    # corner case: empty string
     def test_empty_string(self):
         # you should input wrong data
-        self.assertRaises(KeyError,lambda:get_location(" ") )
+        self.assertRaises(KeyError, lambda: get_location(" "))
 
 if __name__ == '__main__':
     unittest.main()
