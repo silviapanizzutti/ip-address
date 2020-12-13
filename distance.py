c5cdfae64a855199d6aa31b6309b508eb8aacf05
silviapanizzutti <74045150+silviapanizzutti@users.noreply.github.com>
2020-12-13 16:25:28 +0100

    Update distance.py
    adding the docstring

--- distance.py
+++ distance.py
@@ -4,4 +4,5 @@ from geopy import distance
 # this function measures the difference in km
 # from one ip to the other using their coordinates
 def getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2):
+    """Returns the distance between the ip of the user and the ip requested"""
     return distance.distance((lat1, lon1), (lat2, lon2)).km
